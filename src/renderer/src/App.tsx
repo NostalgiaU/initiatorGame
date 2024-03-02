8cf7463b7ec2d1a51153262cc32fdde7d8543de2
Kilig <1599017467@qq.com>
2024-03-02 20:27:18 +0800

    feat: 搭建基础框架

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+import Img1 from './assets/PixPin_2024-03-02_19-28-01.png'
+
+function App(): JSX.Element {
+  return (
+    <div className="h-full w-full">
+      <img src={Img1} alt="" />
+    </div>
+  )
+}
+
+export default App
